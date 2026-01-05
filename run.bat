8cf69e6f9717c8aa1efd67965b8add4535385702
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-01-05 14:39:24 +0100

    make run.bat work even if ran as Admin

--- run.bat
+++ run.bat
@@ -5,5 +5,15 @@
 @rem go run -race main.go
 
 @rem pause
-.\dnsbollocks.exe
+echo "Current working directory is:"
+cd
+echo "Script is running from %~dp0"
+cd %~dp0
+echo "Current(changed) working directory is:"
+cd
+@rem .\dnsbollocks.exe
+
+@rem %~dp0 already has the end \ but adding another one for visibility:
+@echo on
+%~dp0\dnsbollocks.exe
 pause
