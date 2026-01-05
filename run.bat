5d666f7466713de6588e0548ca9b233de44d9f8c
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-01-05 15:05:51 +0100

    fix cd %~dp0 should be cd /d %~dp0 in run.bat
    else for different drives, it won't change current dir!

--- run.bat
+++ run.bat
@@ -8,7 +8,15 @@
 echo "Current working directory is:"
 cd
 echo "Script is running from %~dp0"
-cd %~dp0
+cd /d %~dp0
+:: What %~dp0 actually is
+:: %0 → the path used to launch the script
+:: ~d → drive letter
+:: ~p → path (ending with a backslash)
+::
+:: cd /d changes the driver letter too
+:: "Use the /D switch to change current drive in addition to changing current directory for a drive."
+
 echo "Current(changed) working directory is:"
 cd
 @rem .\dnsbollocks.exe
