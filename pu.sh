a95cf02d4287018cb553593a96086f4e78631ab4
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-06-19 22:51:09 +0200

    a note about lightweight vs annotated git tags

--- pu.sh
+++ pu.sh
@@ -1,4 +1,6 @@
 
 #This tells Git: "Push my current branch commits like normal, and if there are any annotated tags associated with the commits I am pushing, send those up too." Either way, git push --tags is perfectly fine for your first tag push to get it out there!
+#Note: this non-anotateg tag won't be pushed: git tag v0.0.1
+#      but this will be: git tag -a v0.0.1 -m "Release v0.0.1"
 git push --follow-tags
 echo done
