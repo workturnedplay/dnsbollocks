027125286d3f9339d21271d0a00667a4d4bae777
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-06-29 03:05:42 +0200

    git --no-pager on ./pu.sh

--- pu.sh
+++ pu.sh
@@ -1,6 +1,6 @@
 
 #shows all tags as if they were versions
-git tag --sort=version:refname --list 'v*'
+git --no-pager tag --sort=version:refname --list 'v*'
 #This tells Git: "Push my current branch commits like normal, and if there are any annotated tags associated with the commits I am pushing, send those up too." Either way, git push --tags is perfectly fine for your first tag push to get it out there!
 #Note: this non-anotateg tag won't be pushed: git tag v0.0.1
 #      but this will be: git tag -a v0.0.1 -m "Release v0.0.1"
