8dc31b0377dd34aef9616ff5f34d6e60b1a88a3a
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-07-23 17:29:03 +0200

    run tests with race which also checks ptrs

---
+++ testwrace.bat
@@ -0,0 +1,8 @@
+@echo off
+rem 1. Prevent the current working directory from taking precedence over PATH, doesn't work with eg. "start go.exe"
+set "NoDefaultCurrentDirectoryInExePath=1"
+cd /d "%~dp0"
+
+set RUNWITHRACE="-race"
+call .\test.bat
+pause
